43ff4f756478997556b1c4adbb49dfd7068a27e6
bpervaiz <30590595+bpervaiz@users.noreply.github.com>
2018-02-26 18:37:54 +0500

    lab 3

---
+++ lab3sc/homepage.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="lab3sc.homepage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cafe Shop</title>
+  
+</head>
+<body>
+    <form id="homepage1" runat="server">
+        <asp:Button ID="Button1" runat="server" Text="View Menu" OnClick="Button1_Click1" /><br /><br />
+            <asp:ListBox ID="ListBox1" runat="server" Height="324px" Width="772px" ></asp:ListBox>
+        </form>
+   <%-- <table class="style1">
+        <tr>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                <asp:DataList ID="DataList1" runat="server" RepeatColumns="3" 
+                    RepeatDirection="Horizontal" onitemcommand="DataList1_ItemCommand" OnSelectedIndexChanged="DataList1_SelectedIndexChanged">
+                    <ItemTemplate>
+                        <table class="style2">
+                            <tr>
+                                <td class="style10">
+                                    &nbsp;</td>
+                                <td class="style4">
+                                    <asp:Image ID="Image1" runat="server" Height="231px" 
+                                        ImageUrl='C:/Users/Bisma/Desktop/FOODPROJECT/images/slide031' Width="300px" />
+                                </td>
+                                <td class="style6">
+                                    &nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="color: #FFFF66" class="style10">
+                                    &nbsp;</td>
+                                <td class="style4" style="color: #FFFF66">
+                                    <table class="style14">
+                                        <tr>
+                                            <td class="style16">
+                                                Name :
+                                                <asp:Label ID="lblname" runat="server" Text='<%#Eval("pname") %>'></asp:Label>
+                                            </td>
+                                            <td rowspan="2" valign="middle"> 
+                                                <table class="style15">
+                                                    <tr>
+                                                        <td>
+                                                            &nbsp;</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <asp:Button ID="Button1" runat="server" CssClass="btn" Height="30px" 
+                                                                Text="VIEW" Width="70px" CommandArgument='<%#Eval("pid") %>' />
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="style16">
+                                                Price&nbsp; :
+                                                <asp:Label ID="lblprice" runat="server" Text='<%#Eval("price") %>'></asp:Label>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="style6" style="color: #FFFF66">
+                                    &nbsp;</td>
+                            </tr>
+                        </table>
+                    </ItemTemplate>
+                </asp:DataList>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>--%>
+
+</body>
+</html>
---
+++ lab3sc/homepage.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using  System.Data;
+using  System.Data.SqlClient;
+using  System.Configuration;
+
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace lab3sc
+{
+    public partial class homepage : System.Web.UI.Page
+    {
+        //SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    if (!IsPostBack)
+        //    {
+        //        Bind_DataList();
+        //    }
+        //}
+        //public void Bind_DataList()
+        //{
+        //    if (con.State == ConnectionState.Closed)
+        //    {
+        //        con.Open();
+        //    }
+        //    SqlCommand cmd = new SqlCommand("select * from user", con);
+        //    SqlDataAdapter adpData = new SqlDataAdapter(cmd);
+        //    DataTable dt = new DataTable();
+        //    adpData.Fill(dt);
+        //    DataList1.DataSource = dt;
+        //    DataList1.DataBind();
+        //    con.Close();
+        //}
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            string MyConString = "SERVER=localhost;DATABASE=cafe_management;UID=root;PASSWORD=;";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader reader;
+            command.CommandText = "select * from menu";
+            connection.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                ListBox1.Items.Add(row[1]+ "Rs.   " + row[2] );
+                Button dynamicButton = new Button();
+                //dynamicButton.BackColor = Color.Red;
+                //dynamicButton.ForeColor = Color.Blue;
+                dynamicButton.Text = "Order Now";
+                dynamicButton.ID = row[0];
+
+                dynamicButton.Click += new EventHandler(dynamicButton_Click);
+                homepage1.Controls.Add(dynamicButton);
+
+            }
+
+            connection.Close();
+        }
+
+        void dynamicButton_Click(object sender, EventArgs e)
+        {
+            int nolim = 3;
+            string curart = "";
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=cafe_management;";
+            string query = "select * from menu where dish_id ='" + (sender as Button).ID + "'";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.GetInt32(3) >0)
+                        {
+                            databaseConnection.Close();
+                            String nolimquery = "UPDATE menu  SET quantity = quantity -1  WHERE dish_id = '" + (sender as Button).ID + "';";
+                            databaseConnection.Open();
+                            using (MySqlCommand nolimcom = new MySqlCommand(nolimquery, databaseConnection))
+                            {
+
+                                nolimcom.CommandTimeout = 60;
+                                int result1 = nolimcom.ExecuteNonQuery();
+
+                                // Check Error
+                                if (result1 < 0)
+                                    Console.WriteLine("Error updating data!");
+                                else
+                                {
+                                    Console.WriteLine("updated data!");
+                                    //MessageBox.Show("done!");
+                                }
+                                databaseConnection.Close();
+                            }
+                      
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
